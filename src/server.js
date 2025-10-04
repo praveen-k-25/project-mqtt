424908c1773689e59e4984b2a8e67e80e839ae87
SWTDeveloper <sproutwingsdeveloper@gmail.com>
2025-10-04 18:26:03 +0530

    Store published MQTT data in MongoDB
    Adds logic to parse published MQTT packets and insert them into the 'user_locations' collection in MongoDB. Ensures a unique index on user and timestamp fields. Handles errors gracefully and logs inserted data.

--- src/server.js
+++ src/server.js
@@ -2,17 +2,30 @@ require("dotenv").config();
 const http = require("http");
 const aedes = require("aedes");
 const ws = require("websocket-stream");
-const { connectDB } = require("./database");
+const {connectDB, getDB} = require("./database");
 
 connectDB();
 
 const broker = aedes();
 
 const wsServer = http.createServer();
-ws.createServer({ server: wsServer }, broker.handle);
+ws.createServer({server: wsServer}, broker.handle);
 
 broker.on("publish", (packet, client) => {
-  console.log("Packet :", packet);
+  try {
+    let data = packet.payload.toString();
+    let d = JSON.parse(data);
+    let db = getDB();
+    if (db) {
+      let collection = db.collection("user_locations");
+      collection.createIndex({user: 1, timestamp: 1}, {unique: true});
+      collection.insertOne(d);
+      console.log("Data Inserted", d);
+    }
+  } catch (err) {
+    console.log("error :", packet.payload.toString());
+  }
+
   //console.log("CLient :", client);
 });
 
